c791aba0598e1234e65b284a59fbac65c815c0d4
Sagor Rana <sagorranait@gmail.com>
2023-02-07 21:25:39 +0600

    Design of the QuizSlider is done.

--- src/Components/QuizSlider.tsx
+++ src/Components/QuizSlider.tsx
@@ -1,55 +1,65 @@
 import React from 'react';
 import Slider from "react-slick";
+import { Box, IconButton, MobileStepper, Typography } from '@mui/material';
+import NavigateBeforeOutlinedIcon from '@mui/icons-material/NavigateBeforeOutlined';
+import NavigateNextOutlinedIcon from '@mui/icons-material/NavigateNextOutlined';
 import SliderItem from './SliderItem';
+import LinearProgress from '@mui/material/LinearProgress';
 
 const QuizSlider: React.FC = () => {
+  const slider = React.useRef<any>(null);
+  const [activeStep, setActiveStep] = React.useState<number>(1);
 
    const settings = {
-      className: "center",
+      arrows: false,
       infinite: true,
       centerPadding: "60px",
       slidesToShow: 5,
       swipeToSlide: true,
       variableWidth: true,
       afterChange: function(index : number) {
-        console.log(
-          `Slider Changed to: ${index + 1}, background: #222; color: #bada55`
-        );
+        setActiveStep(index+1);
       }
     };
 
   return (
-    <div>
-         
-        <h2>Swipe To Slide</h2>
-        <Slider {...settings}>
+    <Box component='div' className='quizSlider'>
+        <Slider ref={slider} {...settings}>
           <SliderItem/>
-          <div>
-            <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-          <div>
-            <h3>5</h3>
-          </div>
-          <div>
-            <h3>6</h3>
-          </div>
-          <div>
-            <h3>7</h3>
-          </div>
-          <div>
-            <h3>8</h3>
-          </div>
-          <div>
-            <h3>9</h3>
-          </div>
+          <SliderItem/>
+          <SliderItem/>
+          <SliderItem/>
+          <SliderItem/>
+          <SliderItem/>
+          <SliderItem/>
+          <SliderItem/>
+          <SliderItem/>
+          <SliderItem/>
         </Slider>
-      </div>
+        <Box component='div' sx={{display: 'flex', alignItems: 'center', gap: '20px', p: '0px 20px'}}>
+          <Box sx={{ width: '80%', mr: 1 }}>
+            {/* <LinearProgress variant="determinate" value={activeStep+50}  /> */}
+            <MobileStepper
+                variant="progress"
+                steps={11}
+                position="static"
+                activeStep={activeStep}
+                sx={{ width: '100%', flexGrow: 1, backgroundColor: '#F7F8FC' }}
+                nextButton={undefined}
+                backButton={undefined}
+              />
+          </Box>
+          <Typography variant='inherit'>{activeStep}/10</Typography>
+          <Box component='div'>
+            <IconButton sx={{border: '1px solid #D9E5EE', mr: '10px'}} onClick={() => slider?.current?.slickPrev()} color="primary" component="label">
+                <NavigateBeforeOutlinedIcon/>
+            </IconButton>
+            <IconButton sx={{border: '1px solid #D9E5EE'}} onClick={() => slider?.current?.slickNext()} color="primary" component="label">
+                <NavigateNextOutlinedIcon/>
+            </IconButton>
+          </Box>
+        </Box>
+      </Box>
   )
 }
 
