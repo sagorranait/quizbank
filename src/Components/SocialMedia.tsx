624689bd010bbbe6d71437135475d3e1aae1aacb
Sagor Rana <sagorranait@gmail.com>
2023-02-25 23:27:02 +0600

    Functionality of signInWithProup is done.

--- src/Components/SocialMedia.tsx
+++ src/Components/SocialMedia.tsx
@@ -3,9 +3,83 @@ import { Box, Typography, Button, Avatar } from '@mui/material';
 import Google from '../Assets/search.png';
 import FaceBook from '../Assets/facebook.png';
 import GitHub from '../Assets/github.png';
+import {AuthProvider, FacebookAuthProvider, GithubAuthProvider, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { auth, db, Gprovider, Fprovider, GHprovider } from '../firebase.config';
+import { toast } from 'react-hot-toast';
+import { doc, getDoc, setDoc } from 'firebase/firestore';
+import { useAppDispatch } from '../app/store';
+import { storeUser } from '../features/user/user';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+interface PopupProps {
+   Gprovider?: GoogleAuthProvider  | undefined;
+   Fprovider?: FacebookAuthProvider  | undefined;
+   GHprovider?: GithubAuthProvider  | undefined;
+}
+
+interface ExtendedPopupProps extends PopupProps {
+   provider: 'google' | 'facebook' | 'github';
+}
 
 
 const SocialMedia: React.FC = () => {
+   const dispatch = useAppDispatch();
+   let navigate = useNavigate();
+   let location = useLocation();
+
+   let from = location.state?.from?.pathname || "/";
+
+   const signInWithSocialMediaPopup = (props: ExtendedPopupProps) => {
+      let providers: AuthProvider | undefined;
+
+      switch (props.provider) {
+         case 'google':
+           providers = props.Gprovider || new GoogleAuthProvider();   
+           break;
+         case 'facebook':
+           providers = props.Fprovider || new FacebookAuthProvider();   
+           break;
+         case 'github':
+           providers = props.GHprovider || new GithubAuthProvider();   
+           break;
+         default:
+           providers = props.Gprovider || new GoogleAuthProvider();
+           break;
+       }
+
+      console.log(props.provider);
+      
+
+      signInWithPopup(auth, providers)
+      .then(async (result) => {
+         const user = result.user;        
+         if (user) {
+            const { uid } = user;
+            const docRef = doc(db, "user", uid);
+            const docSnap = await getDoc(docRef);
+      
+            if (docSnap.exists()) {
+               const {name, email} = docSnap.data();
+               dispatch(storeUser({id: uid, name, email}));
+            } else{
+               await setDoc(doc(db, "user", user.uid), {
+                  name: user.displayName,
+                  email: user.email,
+                  quiz_took: [],
+                });
+                dispatch(storeUser({id: uid, name: user.displayName, email: user.email}));
+            }
+         }
+
+         navigate(from, { replace: true });
+         toast.success('Successfully Sign-In.');
+      }).catch((error) => {
+         const errorCode = error.code;
+         const errorMessage = error.message;
+         toast.error(`${errorCode} : ${errorMessage}`);
+      });
+   }
+
   return (
    <Box component='div' sx={{m: '20px 0 35px'}}>
       <Typography variant='inherit'>Or</Typography>
@@ -21,6 +95,7 @@ const SocialMedia: React.FC = () => {
          }}
       >
          <Button 
+            onClick={()=>signInWithSocialMediaPopup({Gprovider, provider: 'google'})}
             sx={{
                color: '#000', 
                border: '1px solid #EA4335', 
@@ -35,6 +110,7 @@ const SocialMedia: React.FC = () => {
             Google
          </Button>
          <Button 
+            onClick={()=>signInWithSocialMediaPopup({Fprovider, provider: 'facebook'})}
             sx={{
                color: '#000', 
                border: '1px solid #4267B2', 
@@ -46,6 +122,7 @@ const SocialMedia: React.FC = () => {
             Facebook
          </Button>
          <Button 
+            onClick={()=>signInWithSocialMediaPopup({GHprovider, provider: 'github'})}
             sx={{
                color: '#000', 
                border: '1px solid #000', 
--- src/firebase.config.ts
+++ src/firebase.config.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { FacebookAuthProvider, getAuth, GithubAuthProvider, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -14,5 +14,15 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+const Gprovider = new GoogleAuthProvider();
+const Fprovider = new FacebookAuthProvider();
+const GHprovider = new GithubAuthProvider();
 
-export { auth, db };
+
+export { 
+   auth, 
+   db,
+   Gprovider,
+   Fprovider,
+   GHprovider,
+};
